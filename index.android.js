7878cefbc2d6b03351cd976642325e1506ba04a0
Wonyoung <wonyoungjjang@gmail.com>
2016-03-02 17:06:00 +0900

    Add position control

--- index.android.js
+++ index.android.js
@@ -130,23 +130,32 @@ class PositionControl extends Component {
     super(props);
     this.radiogroup = new MKRadioButton.Group();
   }
+
+  onChecked(position) {
+    return ({checked}) => {
+      if (checked) {
+        console.log(position);
+      }
+    };
+  }
+
   render() {
     return (
       <View style={{flex:2, flexDirection:'column'}}>
         <View style={{flex:1, flexDirection:'row'}}>
-          <MKRadioButton checked={false} onCheckedChange={(e,i)=>{console.log(e,i)}} group={this.radiogroup}/>
-          <MKRadioButton checked={false} onCheckedChange={(e,i)=>{console.log(e,i)}} group={this.radiogroup}/>
-          <MKRadioButton checked={false} onCheckedChange={(e,i)=>{console.log(e,i)}} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(1)} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(2)} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(3)} group={this.radiogroup}/>
         </View>
         <View style={{flex:1, flexDirection:'row'}}>
-          <MKRadioButton checked={false} group={this.radiogroup}/>
-          <MKRadioButton checked={false} group={this.radiogroup}/>
-          <MKRadioButton checked={false} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(4)} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(5)} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(6)} group={this.radiogroup}/>
         </View>
         <View style={{flex:1, flexDirection:'row'}}>
-          <MKRadioButton checked={false} group={this.radiogroup}/>
-          <MKRadioButton checked={false} group={this.radiogroup}/>
-          <MKRadioButton checked={false} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(7)} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(8)} group={this.radiogroup}/>
+          <MKRadioButton checked={false} onCheckedChange={this.onChecked(9)} group={this.radiogroup}/>
         </View>
       </View>
     );
